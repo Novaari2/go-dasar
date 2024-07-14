a7bbeb07301149ba7513bdc41c490302a8fddf18
novaari2 <novaari20@gmail.com>
2024-07-14 10:44:31 +0700

    custom error

--- custom_error.go
+++ custom_error.go
@@ -1,5 +1,48 @@
 package main
 
-type validationError struc{
-	
+import "fmt"
+
+type validationError struct{
+	Message string
+}
+
+type notFountError struct{
+	Message string
+}
+
+func (v *validationError) Error() string{
+	return v.Message
+}
+
+func (n *notFountError) Error() string{
+	return n.Message
+}
+
+func SaveData(id string, data any) error {
+	if id == ""{
+		return &validationError{"Validation Error"}
+	}
+
+	if id != "nova"{
+		return &notFountError{"data not found"}
+	}
+
+	// ok
+
+	return nil
+}
+
+func main(){
+	err := SaveData("nova", nil)
+	if err != nil{
+		if validationError, ok := err.(*validationError); ok{
+			fmt.Println("validation error", validationError.Error())
+		}else if notFountError, ok := err.(*notFountError); ok{
+			fmt.Println("not found error:", notFountError.Error())
+		}else{
+			fmt.Println("unknown error:", err.Error())
+		}
+	}else{
+		fmt.Println("Sukses")
+	}
 }
